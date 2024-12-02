9cfc1fce9ecf7cb97e818d886036cfe1a70acb69
Sajiboahid007 <sajiboahid007@gmail.com>
2024-12-02 07:52:39 +0600

    sidebarupdate

--- sidebar.php
+++ sidebar.php
@@ -1,51 +1,46 @@
+<!-- Sidebar -->
+<nav class="col-md-3 col-lg-2 d-md-block bg-dark sidebar py-4">
+  <div class="position-sticky">
+    <h2 class="px-3">Sidebar</h2>
+    <ul class="nav flex-column px-3">
+      <li class="nav-item">
+        <a class="nav-link" href="./department-list.php">Department Dashboard</a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link link-press"
+          href="./department-list.php">
+          Department List
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link link-press" href="./booklist.php">
+          Book
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link link-press"
+          href="#"
+          url="./accountlist.html">
+          Bank Account
+        </a>
+      </li>
 
-     <!-- Sidebar -->
-     <nav class="col-md-3 col-lg-2 d-md-block bg-dark sidebar py-4">
-       <div class="position-sticky">
-         <h2 class="px-3">Sidebar</h2>
-         <ul class="nav flex-column px-3">
-           <li class="nav-item">
-             <a class="nav-link" href="./dashboard.php">Student Dashboard</a>
-           </li>
-           <li class="nav-item">
-             <a
-               class="nav-link link-press"
-               href="./studentList.php"
-             >
-               Students
-             </a>
-           </li>
-           <li class="nav-item">
-             <a class="nav-link link-press" href="./booklist.php">
-               Book
-             </a>
-           </li>
-           <li class="nav-item">
-             <a
-               class="nav-link link-press"
-               href="#"
-               url="./accountlist.html"
-             >
-               Bank Account
-             </a>
-           </li>
-
-           <li class="nav-item">
-             <a
-               class="nav-link link-press"
-               href="./borrowBook.php"
-             >
-               Book in Hand
-             </a>
-           </li>
-         </ul>
-       </div>
-     </nav>
+      <li class="nav-item">
+        <a
+          class="nav-link link-press"
+          href="./borrowBook.php">
+          Book in Hand
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 
-     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-          <div
-            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
-          ></div>
-            
-          <div id="main-content">
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+  <div
+    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"></div>
+
+  <div id="main-content">
